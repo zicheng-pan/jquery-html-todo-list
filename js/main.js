8474b0932014543813dca080731a42a374c9a1d1
zicheng <zpan@hpe.com>
2018-08-22 18:24:26 +0800

    delete item

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
     'use strict';
     // console.log('jquery',jQuery);
     var $addTaskForm = $('.add_task')
+        ,$delete_index=null
         ,task_list = []
     ;
 
@@ -33,14 +34,14 @@
         store.set('task_list',task_list);
     }
 
-    function renderTpl(data){
+    function renderTpl(data,index){
         var task_item_tpl =
-            '<div class="taskitem">' +
+            '<div class="taskitem" data-index="'+ index +'">' +
             '<span><input type="checkbox" style="margin-right:10px;"></span>' +
             '<span class="task_content">'+data.content+'</span>' +
             '<span class="fr">'+
-            '<span class="action">delete</span>' +
-            '<span class="action">detail</span>' +
+            '<span class="action delete">delete</span>' +
+            '<span class="action detail">detail</span>' +
             '</span>'+
             '</div>';
         // console.log('temp_task_item',task_item_tpl);
@@ -53,9 +54,26 @@
         var $tasklist =  $('.tasklist');
         $tasklist.html('');
         for ( var x=0; x<task_list.length; x++){
-            var $task_item = renderTpl(task_list[x]);
+            var $task_item = renderTpl(task_list[x],x);
             $tasklist.append($task_item);
         }
+        // action 和 delete 是同级
+        $delete_index = $('.action.delete');
+        // console.log('delete_index',$delete_index);
+        $delete_index.on("click",function(){
+            var $this = $(this);
+            var index = $this.parent().parent().data('index');
+            deleteTaskItem(index);
+        })
+
+    }
+
+    //delete task item
+    function deleteTaskItem(index){
+        if( !index || !task_list[index]) return ;
+        delete task_list[index];
+        renderTaskList();
+
     }
 
 })();
