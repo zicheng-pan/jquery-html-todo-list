7c68ddfb15ceb7d55de108d8882ca0db99f13e59
zicheng <zpan@hpe.com>
2018-08-23 20:16:30 +0800

    show details

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@
     // console.log('jquery',jQuery);
     var $addTaskForm = $('.add_task')
         ,$delete_index=null
+        ,$detail_task=null
+        ,$task_detail_mask=$('.task_detail_mask')
+        ,$task_detail_div=$('.task_detail')
+        ,$task_detail_content=$('#task_detail_content')
         ,task_list = []
     ;
 
@@ -22,11 +26,20 @@
         $input.val(null);
     });
 
+    function hideTaskDiv(){
+        $task_detail_mask.hide();
+        $task_detail_div.hide();
+    }
+
     function init(){
         task_list = store.get('task_list') || [];
         if(task_list.length>0){
             renderTaskList()
         }
+        $task_detail_mask.on("click",function(){
+            hideTaskDiv()
+        });
+
     }
 
     function storeTaskList(new_task){
@@ -51,6 +64,41 @@
 
     }
 
+    function renderTaskDetailTpl(index){
+        console.log('index',index);
+
+        var task = task_list[index];
+        // console.log('task.desc === undefined',task.desc === undefined);
+        var task_desc = task.desc === undefined ? "" : task.desc;
+        var task_detail_tpl=
+            '<form>' +
+            '<div class="content">'+task.content+'</div>' +
+            '<textarea name="task_desc" class="desc">'+ task_desc +'</textarea>' +
+            '<div class="remind">' +
+            '<input name="task_date" type="date" value="'+ task.date +'">' +
+            '<button type="submit">submit</button>' +
+            '</div>' +
+            '</form>'
+            ;
+        console.log('task_detail_tpl',task_detail_tpl);
+        $task_detail_content.html(task_detail_tpl);
+        var $task_detail_form = $task_detail_content.find('form');
+        $task_detail_form.on('submit',function(e){
+            e.preventDefault();
+            var task_desc = $task_detail_form.find("[name=task_desc]").val();
+            var task_date = $task_detail_form.find("[name=task_date]").val();
+            // console.log(task_desc,task_date);
+            task.desc = task_desc;
+            task.date = task_date;
+            task_list[index] = task;
+            store.set('task_list',task_list);
+            renderTaskList();
+            hideTaskDiv();
+        });
+
+
+    }
+
     function renderTaskList(){
 
         var $tasklist =  $('.tasklist');
@@ -70,7 +118,21 @@
             temp ? deleteTaskItem(index) : null;
             // if(deleteTaskItem(index))
             //     alert('delete successfully');
-        })
+        });
+
+        //add detail button click event
+        $detail_task = $('.action.detail');
+        $detail_task.on("click",function(){
+            var $this = $(this);
+            var index = $this.parent().parent().data('index');
+            // console.log('index',index);
+            // 设置content detail div的样式
+            renderTaskDetailTpl(index);
+            // 点击并且弹框
+            $task_detail_mask.show();
+            $task_detail_div.show();
+        });
+
 
     }
 
